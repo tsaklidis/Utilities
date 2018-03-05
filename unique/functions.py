d18cfe0f9b4046126504fe778b0d7e472d6e1f13
Stefanos I. Tsaklidis <ctefanos.t@gmail.com>
2018-03-05 16:34:45 +0200

    Some extra checks

--- unique/functions.py
+++ unique/functions.py
@@ -6,6 +6,8 @@ import datetime
 # Don't overload your system over get_random_string(1000000)
 def get_random_string(length=8, prefix='', suffix='', invalid_chars=False):
     u = ''
+    if (not isinstance(length, int)) or length < 4:
+        length = 8
     while True:
         if len(u) < length:
             salt = hashlib.sha256(str(datetime.datetime.now())).hexdigest()

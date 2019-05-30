b1ee17872bc5797136e0870ba4798cf5e9bef6b2
Stefanos I. Tsaklidis <ctefanos.t@gmail.com>
2019-05-30 19:00:58 +0300

    Paths and some python2 to python3 changes

--- utilities/send_mail/README.md
+++
@@ -1,3 +0,0 @@
-<h4>How send custom email</h4>
-Django is used for this function
-<hr>
--- utilities/unique.py
+++ utilities/unique.py
@@ -4,13 +4,14 @@ import datetime
 
 # Create a random string based on timestamp
 # Don't overload your system over get_random_string(1000000)
-def get_random_string(length=8, prefix='', suffix='', invalid_chars=False):
+def get(length=8, prefix='', suffix='', invalid_chars=False):
     u = ''
     if (not isinstance(length, int)) or length < 4:
         length = 8
     while True:
         if len(u) < length:
-            salt = hashlib.sha256(str(datetime.datetime.now())).hexdigest()
+            the_moment = str(datetime.datetime.now()).encode('utf-8')
+            salt = hashlib.sha256(the_moment).hexdigest().encode('utf-8')
             u += hashlib.sha1(salt).hexdigest()
         else:
             break
